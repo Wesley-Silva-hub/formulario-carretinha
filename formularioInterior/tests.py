ce581959b5769a1a834234d02d25c7164bdace55
Wesley-Silva-hub <josewesley12332172@gmail.com>
2024-03-09 20:08:35 -0300

    refazendo o requeriments.txt para o django na versao 5.0.3

--- formularioInterior/tests.py
+++ formularioInterior/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+# No arquivo tests.py
 
-# Create your tests here.
+import unittest
+from django.test import TestCase
+from .models import Pessoa 
+
+class PessoaTestCase(TestCase):
+
+    def setUp(self):
+        Pessoa.objects.create(nome="Exemplo Pessoa", cpf="123.456.789-00")
+
+    def test_pessoa_nome(self):
+        pessoa = Pessoa.objects.get(nome="Exemplo Pessoa")
+        self.assertEqual(pessoa.nome, "Exemplo Pessoa")
+
+    def test_pessoa_cpf(self):
+        pessoa = Pessoa.objects.get(cpf="123.456.789-00")
+        self.assertEqual(pessoa.cpf, "123.456.789-00")
+
+if __name__ == '__main__':
+    unittest.main()
